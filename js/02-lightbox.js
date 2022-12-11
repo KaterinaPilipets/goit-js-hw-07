b0b12dfb91b3da022310f785eeffa9b3d91b4c9d
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-11 21:54:50 +0200

    fix task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ gallery.insertAdjacentHTML("beforeend", createGaleryItems(galleryItems));
 function onGalleryClick(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") return;
-  let galleryLightbox = new SimpleLightbox(".gallery__item", {
+  let galleryLightbox = new SimpleLightbox(".gallery__link", {
     captionsData: "alt",
     captionDelay: 250,
   });
@@ -19,9 +19,38 @@ function createGaleryItems(galleryItemsArr) {
   return galleryItemsArr
     .map(
       ({ preview, original, description }) => `
-          <a class="gallery__item" href="${original}">
+      <li class= "gallery__item">
+          <a class="gallery__link" href="${original}">
              <img class="gallery__image" src="${preview}" alt="${description}"/>
-         </a>`
+         </a></li>`
     )
     .join("");
 }
+
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+
+// const gallery = document.querySelector(".gallery");
+
+// gallery.addEventListener("click", onGalleryClick);
+// gallery.insertAdjacentHTML("beforeend", createGaleryItems(galleryItems));
+
+// function onGalleryClick(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") return;
+//   let galleryLightbox = new SimpleLightbox(".gallery__item", {
+//     captionsData: "alt",
+//     captionDelay: 250,
+//   });
+// }
+
+// function createGaleryItems(galleryItemsArr) {
+//   return galleryItemsArr
+//     .map(
+//       ({ preview, original, description }) => `
+//           <a class="gallery__item" href="${original}">
+//              <img class="gallery__image" src="${preview}" alt="${description}"/>
+//          </a>`
+//     )
+//     .join("");
+// }
