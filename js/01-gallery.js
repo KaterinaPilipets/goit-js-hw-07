2f0dee20049224c8be91cf3379c55cb95cc70814
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-11 21:32:00 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+gallery.addEventListener("click", onGalleryClick);
+gallery.insertAdjacentHTML("beforeend", createGaleryItems(galleryItems));
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" />`,
+    { onShow: (instance) => onShowInstance(instance) }
+  );
+  instance.show();
+}
+function onShowInstance(instance) {
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function createGaleryItems(galleryItemsArr) {
+  return galleryItemsArr
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+         </a>
+    </div>`
+    )
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onGalleryClick);
+gallery.insertAdjacentHTML("beforeend", createGaleryItems(galleryItems));
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  let galleryLightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
+
+function createGaleryItems(galleryItemsArr) {
+  return galleryItemsArr
+    .map(
+      ({ preview, original, description }) => `
+          <a class="gallery__item" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a>`
+    )
+    .join("");
+}
